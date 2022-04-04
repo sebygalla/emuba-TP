47f32f8357309edf4082e9b1cbc3d903b6731821
sebygalla <sebygalla@hotmail.com>
2022-04-03 23:53:26 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-//captura de boton VER MAS!
+
 const btnOrar = document.getElementById('oracion');
-//captura de id modal
 const modal = document.getElementById('modal');
-//captura de boton cerrar
 const btnCerrar = document.getElementById('verMas');
 
 
 
 btnOrar.addEventListener('click', () => {
-    modal.classList.add('modal__contenedor--activo');
-  
-   
+    modal.classList.add('modal__contenedor--activo'); 
 });
 
 btnCerrar.addEventListener('click', () => {
     modal.classList.remove('modal__contenedor--activo');
-  
 });
 
